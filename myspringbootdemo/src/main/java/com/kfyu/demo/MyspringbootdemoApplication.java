e6bb283d8d464e9fb4ed255c79741bf06c56e784
kaifengyu <kaifeng.yu@kpmg.com>
2018-11-23 17:10:56 +0800

    init

---
+++ myspringbootdemo/src/main/java/com/kfyu/demo/MyspringbootdemoApplication.java
@@ -0,0 +1,12 @@
+package com.kfyu.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyspringbootdemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyspringbootdemoApplication.class, args);
+	}
+}
